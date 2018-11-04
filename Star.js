d2b10c0a4a0d619cd21c7d76af6b6e92a5f9c644
hthg13 <32647181+hthg13@users.noreply.github.com>
2018-11-04 21:01:56 +0000

    bugfixes: stjarna var stundum útaf, ekki lengur

--- Star.js
+++ Star.js
@@ -19,8 +19,8 @@ function Star(descr) {
     //set x position
     var widthOfPlat = entityManager._platforms[1].width;    //get the width of the platform
     var xPos = entityManager._platforms[1].x;               //get the starting x position of the platform
-    var randPosX = util.randRange(xPos, widthOfPlat);       //get random position for the star
-    this.x = randPosX+widthOfPlat-this.width;               //set the position of the star
+    var randPosX = util.randRange(xPos, xPos+widthOfPlat);  //get random position for the star
+    this.x = randPosX-this.width;                           //set the position of the star
 
     //set y position
     var yPos = entityManager._platforms[1].y - this.height; //get the y position of the platform
